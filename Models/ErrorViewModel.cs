793f6b4c5a391e83d84f9fc4c1182fd25443b13a
黃敬敦 <promise52252@gmail.com>
2024-09-15 11:33:48 +0800

    'init'

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ActivitySample.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
